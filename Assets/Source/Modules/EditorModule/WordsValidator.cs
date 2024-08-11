8fad1068af5f4206191e794d55ab1f0fd9c5cbd7
madeflyoke <sano555000@gmail.com>
2024-08-11 21:39:42 +0300

    upd: words validation extended by existing api lookup

--- Assets/Source/Modules/EditorModule/WordsValidator.cs
+++ Assets/Source/Modules/EditorModule/WordsValidator.cs
@@ -1,11 +1,14 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Cysharp.Threading.Tasks;
 using EasyButtons;
 using Source.Modules.GameLogicModule.Scripts.Levels;
 using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Source.Modules.EditorModule
 {
@@ -15,10 +18,12 @@ namespace Source.Modules.EditorModule
         [SerializeField] private int _readonly_totalWordsCount;
 
         #region WordsValidationAndRecreationRegion
-
+        
         [Button]
-        private void ValidateWordsJson()
+        private async void ValidateWordsJson() //Long waiting required (probable some minutes)
         {
+            Debug.Log("Validating started...");
+            
             var wordsFetcher = new WordsFetcher();
             var allWords = wordsFetcher.GetAllWords();
             wordsFetcher.Dispose();
@@ -27,24 +32,105 @@ namespace Source.Modules.EditorModule
             
             allWords = ValidateWordsLength(allWords);
             allWords = ValidateRepeats(allWords);
+            
+            allWords = await ValidateExisting(allWords);
 
             if (originWordsCount!=allWords.Count)
             {
-                Debug.LogWarning("Changes were made, words removed: "+(originWordsCount-allWords.Count));
+                Debug.Log("Changes were made, words removed: "+(originWordsCount-allWords.Count));
             }
             SaveProcessedFile(new WordsModel(allWords));
+            
+            DebugLogWithColor("Validating completed!", Color.green);
         }
 
         private List<string> ValidateWordsLength(List<string> words)
         {
-            return words.Where(x => x.Length is >= 4 and <= 7).ToList();
+            Debug.Log("Length validating started...");
+
+            var finalWords = words.Where(x => x.Length is >= 4 and <= 7).ToList();
+            
+            DebugLogWithColor("Length validating completed!", Color.green);
+
+            return finalWords;
         }
 
         private List<string> ValidateRepeats(List<string> words)
         {
-            return new HashSet<string>(words).ToList();
+            Debug.Log("Repeats validating started...");
+
+            var finalWords = new HashSet<string>(words).ToList();
+            
+            DebugLogWithColor("Repeats validating completed!", Color.green);
+
+            return finalWords;
         }
-    
+
+        //HEAVY API OPERATION
+        private async UniTask<List<string>> ValidateExisting(List<string> words)
+        {
+            Debug.Log("Repeats validating started, be patient...");
+
+            async UniTask<bool> CheckWord(string word, string languageCode)
+            {
+                var struckTimerSeconds = TimeSpan.TicksPerSecond * 30;
+                var startTime = DateTime.UtcNow.Ticks;
+                
+                string url =
+                    $"https://{languageCode}.wiktionary.org/w/api.php?action=query&titles={UnityWebRequest.EscapeURL(word)}" +
+                    $"&format=json&prop=extracts&explaintext&exsectionformat=plain";
+
+                using (UnityWebRequest request = UnityWebRequest.Get(url))
+                {
+                    var operation = request.SendWebRequest();
+                    
+                    while (operation.isDone == false)
+                    {
+                        if (startTime+struckTimerSeconds<=DateTime.UtcNow.Ticks)
+                        {
+                            Debug.LogError($"Timed out at word {word}!");
+                            return false;
+                        }
+                        await UniTask.Yield();
+                    }
+
+                    if (request.result == UnityWebRequest.Result.ConnectionError ||
+                        request.result == UnityWebRequest.Result.ProtocolError)
+                    {
+                        Debug.LogError(request.error);
+                    }
+                    else
+                    {
+                        var json = request.downloadHandler.text;
+                        if (json.Contains("\"-1\""))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            List<string> finalWords = new List<string>(words);
+            
+            foreach (var word in words)
+            {
+                Debug.Log($"Checking the word: {word}...");
+                bool result = await CheckWord(word, "ru");
+                if (result==false)
+                {
+                    finalWords.Remove(word);
+                    DebugLogWithColor($"Removed word: {word}", Color.yellow);
+                }
+            }
+            
+            DebugLogWithColor("Repeats validating completed!", Color.green);
+
+            return finalWords;
+        }
+
         private void SaveProcessedFile(WordsModel wordsModel)
         {
             string json = JsonConvert.SerializeObject(wordsModel, Formatting.Indented);
@@ -62,6 +148,7 @@ namespace Source.Modules.EditorModule
         [Button]
         public void FillWordsData()
         {
+            Debug.Log("Filling words data started...");
             _readonly_wordsValidationDatas = new List<WordsRequestData>();
             
             var wordsFetcher = new WordsFetcher();
@@ -83,9 +170,16 @@ namespace Source.Modules.EditorModule
             }
             
             _readonly_totalWordsCount = _readonly_wordsValidationDatas.Sum(x => x.WordsCount);
+            DebugLogWithColor("Filling words data completed!", Color.green);
         }
         
         #endregion
+        
+        private void DebugLogWithColor(string message, Color color)
+        {
+            string colorHex = ColorUtility.ToHtmlStringRGB(color);
+            Debug.Log($"<color=#{colorHex}>{message}</color>");
+        }
     }
 }
 
