fb513fe36c149c196e10f9b80cdae153d42c9970
madeflyoke <sano555000@gmail.com>
2024-08-15 12:11:31 +0300

    upd: some animation

--- Assets/Source/Modules/GameLogicModule/Scripts/Words/WordView.cs
+++ Assets/Source/Modules/GameLogicModule/Scripts/Words/WordView.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Source.Modules.GameLogicModule.Scripts.Words
@@ -5,10 +7,30 @@ namespace Source.Modules.GameLogicModule.Scripts.Words
     public class WordView : MonoBehaviour
     {
         [SerializeField] private GameObject _completedWordBg;
+        private Tween _tween;
+        private Vector3 _defaultCompletedWordBgScale;
+        
+        private void Awake()
+        {
+            _defaultCompletedWordBgScale = _completedWordBg.transform.localScale;
+        }
 
         public void SetViewState(bool isWordCompleted)
         {
-            _completedWordBg.SetActive(isWordCompleted);
+            if (isWordCompleted)
+            {
+                _completedWordBg.transform.localScale = _defaultCompletedWordBgScale * 1.2f;
+                _completedWordBg.SetActive(true);
+                _tween?.Kill();
+                _tween = _completedWordBg.transform.DOScale(_defaultCompletedWordBgScale, 0.15f).SetEase(Ease.InQuad);
+            }
+            else
+            {
+                _completedWordBg.transform.localScale = _defaultCompletedWordBgScale;
+                _completedWordBg.SetActive(true);
+                _tween?.Kill();
+                _tween = _completedWordBg.transform.DOScale(_defaultCompletedWordBgScale *1.2f, 0.15f).SetEase(Ease.OutQuad);
+            }
         }
     }
     
